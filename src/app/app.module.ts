c3efc45469fba78d9da3c9f67ea3bf94b361eab6
Martina Kraus <kraus.martina.m@gmail.com>
2024-06-07 11:11:37 +0200

    solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BookCardComponent } from './book-card/book-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookCardComponent
   ],
   imports: [
     BrowserModule
