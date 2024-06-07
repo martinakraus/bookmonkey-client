e485590f235750b57a4add8d093815c588e537eb
Martina Kraus <kraus.martina.m@gmail.com>
2024-06-07 14:27:18 +0200

    Task7_BookApiService

---
+++ src/app/book/book-api.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Book } from "./book";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookApiService {
+  books: Book[] = [
+    {
+      title: "Design Patterns",
+      abstract: "Elements of Reusable Object-Oriented Software",
+      author: "Martina Kraus",
+      isbn: "1"
+    },
+    {
+      title: "REST und HTTP",
+      abstract: "Entwicklung und Integration nach dem Architekturstil des Web",
+      author: "Martina Kraus",
+      isbn: "2"
+    },
+    {
+      title: "Eloquent JavaScript",
+      abstract: "A Modern Introduction to Programming",
+      author: "Martina Kraus",
+      isbn: "3"
+    }
+  ]
+
+  getAll() {
+    return this.books;
+  }
+}
--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Book } from "./book";
+import { BookApiService } from "./book-api.service";
 
 @Component({
   selector: 'app-book',
@@ -7,26 +8,11 @@ import { Book } from "./book";
   styleUrl: './book.component.scss'
 })
 export class BookComponent {
-  books: Book[] = [
-    {
-      title: "Design Patterns",
-      abstract: "Elements of Reusable Object-Oriented Software",
-      author: "Martina Kraus",
-      isbn: "1"
-    },
-    {
-      title: "REST und HTTP",
-      abstract: "Entwicklung und Integration nach dem Architekturstil des Web",
-      author: "Martina Kraus",
-      isbn: "2"
-    },
-    {
-      title: "Eloquent JavaScript",
-      abstract: "A Modern Introduction to Programming",
-      author: "Martina Kraus",
-      isbn: "3"
-    }
-  ]
+  books: Book[];
+
+  constructor(private readonly bookApiService: BookApiService) {
+    this.books = bookApiService.getAll();
+  }
 
   goToBookDetails(book: Book) {
     console.log('Navigate to book details, soon...');
